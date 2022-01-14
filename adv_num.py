d2d280745d4273b4ad402180519c5c84f43d9a76
Miguel Tuda <mituconservata@gmail.com>
2022-01-14 18:11:38 +0100

    codigo reducido a la mitadgit add adv_num.py!

--- adv_num.py
+++ adv_num.py
@@ -3,39 +3,20 @@
 import random
 
 print('¡Hola! ¿Cómo te llamas?')
-nombre = input()
-      
+nombre = input()   
 print('Bueno,', nombre, 'estoy pensando un número entre el 1 y 20, ¿puedes adivinarlo? Tienes 6 intentos.')
 
-num = random.randint(1, 20)
-intentos = 0
-while intentos < 6:
-    print('Intenta adivinar el número')
-    adv = input()
-    while adv.isdigit() == False:
-        print('No has introducido un número. Intenta adivinar el número')
-        adv = input()
-    adv = int(adv)
-
-    intentos += 1
-
-    if adv < num:
-        print('Estoy pensando en un número más alto')
-    elif adv > num:
-        print('Estoy pensando en un número más bajo')
-    elif adv == num:
-        print('¡Muy bien!', nombre, 'has adivinado el número en', intentos, 'intentos.')
-        break
-    elif adv != num:
-        print('Fallaste. El número que estaba pensando era el', num)
+seguir = True
 
 def adv_num():
+    
     """
     Esta función repite el código anterior y solo sera
     llamada si el jugador elige echar otra partida
     """
     num = random.randint(1, 20)
     intentos = 0
+
     while intentos < 6:
         print('Intenta adivinar el número')
         adv = input()
@@ -58,11 +39,13 @@ def adv_num():
     print('¿Quieres jugar de nuevo? (Si / No)')
     jugar = input()
     if jugar == 'Si' or jugar == 'S' or jugar == 's':
+        seguir = True
         adv_num()
+
+    else:
+        seguir = False
 
     return None
 
-print('¿Quieres jugar de nuevo? (Si / No)')
-jugar = input()
-if jugar == 'Si' or jugar == 'S' or jugar == 's':
+if seguir == True:
     adv_num()
